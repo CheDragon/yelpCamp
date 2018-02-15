70979a9b8baac61407a1580439cd674799d98cd0
Rene Trujillo <chedragon18@gmail.com>
2018-02-15 20:33:27 +0000

    update DATABASEURL env variable

--- app.js
+++ app.js
@@ -17,8 +17,11 @@ var indexRoutes         = require("./routes/index"),
     campgroundsRoutes   = require("./routes/campgrounds");
     
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://rene:assassin@ds237748.mlab.com:37748/yelpcamp", { useMongoClient: true });  
+mongoose.connect(process.env.DATABASEURL, { useMongoClient: true });  
+
+//mongoose.connect("mongodb://rene:assassin@ds237748.mlab.com:37748/yelpcamp", { useMongoClient: true });  
 //mongoose.connect("mongodb://localhost/yelp_camp_price", { useMongoClient: true });  
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
